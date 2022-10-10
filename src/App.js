7e7b6b768a8d525adae5bb5836a9b5e07fe4ba8f
Stanlisberg <nnabugo2010@gmail.com>
2022-10-10 08:22:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [answer, setAnswer] = useState('0');
+
+  const clickButton = (e) => {
+    setAnswer(answer.concat(e.target.value));
+  }
+
+  const clearButton = () => {
+    setAnswer('');
+  }
+
+  const backSpace = () => {
+     setAnswer(answer.slice(0, -1))                             
+  }
+
+  const evaluate = () => {
+    setAnswer(eval(answer).toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className ="container">
+        <form>
+          <input type='text' value={answer} />
+        </form>
+
+        <div className='button'                                                                                                                                                                                                                                                                                                                                       >
+          <button className='change-color' id='clear-button' onClick={clearButton}>Clear</button>
+          <button className='change-color' id='back-space' onClick={backSpace}>C</button>
+          <button className='change-color' value='/' onClick={clickButton}>&divide;</button>
+          <button value='7' onClick={clickButton}>7</button>
+          <button value='8'onClick={clickButton}>8</button>
+          <button value='9' onClick={clickButton}>9</button>
+          <button className='change-color' value='*' onClick={clickButton}>&times;</button>
+          <button value='4'onClick={clickButton}>4</button>
+          <button value='5' onClick={clickButton}>5</button>
+          <button value='6' onClick={clickButton}>6</button>
+          <button className='change-color' value='-' onClick={clickButton}>-</button>
+          <button value='1' onClick={clickButton}>1</button>
+          <button value='2' onClick={clickButton}>2</button>
+          <button value='3' onClick={clickButton}>3</button>
+          <button className='change-color' value='+' onClick={clickButton}>+</button>
+          <button value='0' onClick={clickButton}>0</button>
+          <button value='.' onClick={clickButton}>.</button>
+          <button className='change-color' id='answer' onClick={evaluate}>=</button>
+        </div>
+      </div>
+    </>
   );
 }
 
