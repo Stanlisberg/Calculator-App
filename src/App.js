e75ab3160ff297a424ff8123496e7131c9f96aa4
Stanlisberg <nnabugo2010@gmail.com>
2022-10-14 09:45:20 -0700

    Created two component and fatorized codes.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import React, {useState} from 'react'
+import CalculatorContainer from "./components/CalculatorContainer"
 
 function App() {
-  const [answer, setAnswer] = useState('0');
-
-  const clickButton = (e) => {
-    setAnswer(answer.concat(e.target.value));
-  }
-
-  const clearButton = () => {
-    setAnswer('');
-  }
-
-  const backSpace = () => {
-     setAnswer(answer.slice(0, -1))                             
-  }
-
-  const evaluate = () => {
-    setAnswer(eval(answer).toString())
-  }
-
-  return (
+  return(
     <>
-      <div className ="container">
-        <form>
-          <input type='text' value={answer} />
-        </form>
-
-        <div className='button'                                                                                                                                                                                                                                                                                                                                       >
-          <button className='change-color' id='clear-button' onClick={clearButton}>Clear</button>
-          <button className='change-color' id='back-space' onClick={backSpace}>C</button>
-          <button className='change-color' value='/' onClick={clickButton}>&divide;</button>
-          <button value='7' onClick={clickButton}>7</button>
-          <button value='8'onClick={clickButton}>8</button>
-          <button value='9' onClick={clickButton}>9</button>
-          <button className='change-color' value='*' onClick={clickButton}>&times;</button>
-          <button value='4'onClick={clickButton}>4</button>
-          <button value='5' onClick={clickButton}>5</button>
-          <button value='6' onClick={clickButton}>6</button>
-          <button className='change-color' value='-' onClick={clickButton}>-</button>
-          <button value='1' onClick={clickButton}>1</button>
-          <button value='2' onClick={clickButton}>2</button>
-          <button value='3' onClick={clickButton}>3</button>
-          <button className='change-color' value='+' onClick={clickButton}>+</button>
-          <button value='0' onClick={clickButton}>0</button>
-          <button value='.' onClick={clickButton}>.</button>
-          <button className='change-color' id='answer' onClick={evaluate}>=</button>
-        </div>
-      </div>
+     <CalculatorContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/CalculatorContainer.jsx
@@ -0,0 +1,13 @@
+import CalculatorItem from "./CalculatorItem";
+
+function CalculatorContainer() {
+  return (
+    <>
+      <div className ="container">
+        <CalculatorItem />
+      </div>
+    </>
+  );
+}
+
+export default CalculatorContainer;
